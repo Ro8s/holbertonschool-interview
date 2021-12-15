81f48665ee7c109e0ddc48609cfd1312bf91f034
Ro8s <rodrigo.delgado3132@gmail.com>
2021-12-15 15:23:43 +0000

    added some files

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+
+
+
+
+}
