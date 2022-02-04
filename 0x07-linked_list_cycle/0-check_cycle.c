419e59d4a7a35dc96cea87ea2fd21ac82ca1f8d2
Ro8s <rodrigo.delgado3132@gmail.com>
2022-02-04 02:39:36 +0000

    added project

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list is cycle
+ * @list: linked list
+ * Return: 1 or 0
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *a, *b;
+
+a = list;
+b = list;
+while (a && b && b->next)
+{
+a = a->next;
+b  = b->next->next;
+if (s == f)
+return (1);
+}
+return (0);
+}
