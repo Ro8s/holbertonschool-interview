f1f87f034990a9963617f7a89abc2f8090d56920
Ro8s <rodrigo.delgado3132@gmail.com>
2022-04-18 00:31:10 +0000

    fix

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -5,14 +5,14 @@
 def rain(walls):
     """ get water """
     wat = 0
-    if len(walls) < 1:
+    if len(walls) < 1 or type(walls) is not list:
         return wat
     for i in range(len(walls)):
         l = walls[i]
         for j in range(i):
-            left = max(l, walls[j])
+            l = max(l, walls[j])
         r = walls[i]
         for j in range(i + 1, len(walls)):
-            right = max(r, walls[j])
+            r = max(r, walls[j])
         wat += min(l, r) - walls[i]
     return wat
